f38c736b66ae65166aef957e47a09832510f5f92
Eduardo Cerioni Cappellotto <eduardocappellotto@gmail.com>
2022-10-21 16:45:46 -0300

    primeiro teste realizado com sucesso

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,19 @@ const { resolve } = require('path');
 const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
-  displayName: 'root-tests',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
-  testEnvironment: 'node',
+  displayName: "root-tests",
+  testMatch: ["<rootDir>/src/**/*.test.ts"],
+  testEnvironment: "node",
   clearMocks: true,
-  preset: 'ts-jest',
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json"
+    }
+  },
   moduleNameMapper: {
-    '@src/(.*)': '<rootDir>/src/$1',
-    '@test/(.*)': '<rootDir>/test/$1',
-  }
-};
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+}
