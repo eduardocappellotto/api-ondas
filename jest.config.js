6bc8e47faf65c0418d9ec6e2237fa4c1e3e1a90d
Eduardo Cerioni Cappellotto <eduardocappellotto@gmail.com>
2022-10-25 02:21:55 -0300

    feat: adicionado fixtures e mocks do primeiro request criado

--- jest.config.js
+++ jest.config.js
@@ -2,19 +2,21 @@ const { resolve } = require('path');
 const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
-  displayName: "root-tests",
-  testMatch: ["<rootDir>/src/**/*.test.ts"],
-  testEnvironment: "node",
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testEnvironment: 'node',
   clearMocks: true,
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json"
-    }
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
   moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1",
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
   },
-}
+  transformIgnorePatterns: ['node_modules/(?!axios)'],
+};
